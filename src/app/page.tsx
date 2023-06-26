e78a054779ee611c0d18c88ce12f47a590331f93
bisari <bisari31@gmail.com>
2023-06-27 01:02:58 +0900

    design: layout css 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ import Header from '@/components/Header';
 
 export default function page() {
   return (
-    <div className="w-full h-full flex flex-col justify-center items-center">
+    <div className="absolute left-1/2 top-1/2 -translate-y-2/4 -translate-x-2/4">
+      {/* <div className="w-full h-full flex flex-col justify-center items-center"> */}
       <Header />
       <Board />
     </div>
